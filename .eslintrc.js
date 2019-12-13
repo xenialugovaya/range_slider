ece3d7fa8ba6f2a2e537d0db64db8aae185ffff2
Ksenia Lugovaya <xenia.trofimova@gmail.com>
2019-12-13 19:50:32 +0300

    Set up typescript compiler & ESlint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    parserOptions: {
+      ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+      sourceType: "module" // Allows for the use of imports
+    },
+    rules: {
+      // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+      // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    }
+  };
---
+++ sliderPlugin.ts
@@ -0,0 +1,2 @@
+const test = 'wow';
+let newtest
