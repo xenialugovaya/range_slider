86b3988e02205aab842a0e84c1b6f10cb33de40f
Ksenia Lugovaya <xenia.trofimova@gmail.com>
2020-05-12 12:34:16 +0300

    #5 refactored Model constructor

--- src/model/MainModel.ts
+++ src/model/MainModel.ts
@@ -2,31 +2,30 @@ import EventObserver from '../observer/observer';
 import { sliderOptions } from './sliderOptions';
 
 export default class MainModel {
-  public observer: EventObserver;
+  public observer = new EventObserver();
 
-  private minValue = 0;
-
-  private maxValue = 100;
-
-  private stepValue = 1;
-
-  private values = [10, 20];
-
-  private vertical = false;
-
-  private range = true;
-
-  private labels = true;
+  private options = {
+    min: 0,
+    max: 100,
+    step: 1,
+    values: [10, 20],
+    isVertical: false,
+    hasRange: true,
+    hasLabels: true,
+  }
 
   constructor(options: sliderOptions) {
-    this.observer = new EventObserver();
-    this.minValue = options.min ? options.min : this.minValue;
-    this.maxValue = options.max ? options.max : this.maxValue;
-    this.stepValue = options.step ? options.step : this.stepValue;
-    this.values = options.values ? options.values : this.values;
-    this.range = options.hasRange ? options.hasRange : this.range;
-    this.vertical = options.isVertical ? options.isVertical : this.vertical;
-    this.labels = options.hasLabels ? options.hasLabels : this.labels;
+    this.init(options);
+  }
+
+  private init(options: sliderOptions): void {
+    this.options.max = options.max ? options.max : this.options.max;
+    this.options.min = options.min ? options.min : this.options.min;
+    this.options.step = options.step ? options.step : this.options.step;
+    this.options.values = options.values ? options.values : this.options.values;
+    this.options.hasRange = options.hasRange ? options.hasRange : this.options.hasRange;
+    this.options.isVertical = options.isVertical ? options.isVertical : this.options.isVertical;
+    this.options.hasLabels = options.hasLabels ? options.hasLabels : this.options.hasLabels;
   }
 
   update(valueData: sliderOptions): void {
@@ -44,14 +43,14 @@ export default class MainModel {
   }
 
   getMin(): number {
-    if (this.minValue > this.maxValue) {
-      return this.maxValue;
+    if (this.options.min > this.options.max) {
+      return this.options.max;
     }
-    return this.minValue;
+    return this.options.min;
   }
 
   setMin(min: number): void {
-    this.minValue = min;
+    this.options.min = min;
     this.notifyPresenter({
       min: this.getMin(),
       max: this.getMax(),
@@ -61,17 +60,17 @@ export default class MainModel {
   }
 
   getMax(): number {
-    if (this.maxValue < this.minValue) {
-      return this.minValue;
+    if (this.options.max < this.options.min) {
+      return this.options.min;
     }
-    if (this.maxValue === this.minValue) {
-      return this.maxValue + this.stepValue;
+    if (this.options.max === this.options.min) {
+      return this.options.max + this.options.step;
     }
-    return this.maxValue;
+    return this.options.max;
   }
 
   setMax(max: number): void {
-    this.maxValue = max;
+    this.options.max = max;
     this.notifyPresenter({
       min: this.getMin(),
       max: this.getMax(),
@@ -81,12 +80,12 @@ export default class MainModel {
   }
 
   getStep(): number {
-    this.stepValue = this.verifyStep(this.stepValue);
-    return this.stepValue;
+    this.options.step = this.verifyStep(this.options.step);
+    return this.options.step;
   }
 
   setStep(step: number): void {
-    this.stepValue = this.verifyStep(step);
+    this.options.step = this.verifyStep(step);
     this.notifyPresenter({
       step: this.getStep(),
       values: this.getValues(),
@@ -94,31 +93,31 @@ export default class MainModel {
   }
 
   getValues(): number[] {
-    this.values = this.values.map((value) => this.verifyValue(value));
+    this.options.values = this.options.values.map((value) => this.verifyValue(value));
     this.verifyMinMaxValues();
     this.verifyLimits();
-    return this.values;
+    return this.options.values;
   }
 
   setValues(values: number[]): void {
     values.forEach((value, index) => {
-      if (value !== this.values[index]) {
-        this.values[index] = this.verifyValue(value);
+      if (value !== this.options.values[index]) {
+        this.options.values[index] = this.verifyValue(value);
       }
     });
     this.verifyMinMaxValues();
     this.verifyLimits();
     this.notifyPresenter({
-      values: this.values,
+      values: this.options.values,
     });
   }
 
   getOrientation(): boolean {
-    return this.vertical;
+    return this.options.isVertical;
   }
 
   setOrientation(vertical: boolean): void {
-    this.vertical = vertical;
+    this.options.isVertical = vertical;
     this.notifyPresenter({
       values: this.getValues(),
       isVertical: this.getOrientation(),
@@ -126,11 +125,11 @@ export default class MainModel {
   }
 
   getRange(): boolean {
-    return this.range;
+    return this.options.hasRange;
   }
 
   setRange(range: boolean): void {
-    this.range = range;
+    this.options.hasRange = range;
     this.notifyPresenter({
       values: this.getValues(),
       hasRange: this.getRange(),
@@ -138,11 +137,11 @@ export default class MainModel {
   }
 
   getLabels(): boolean {
-    return this.labels;
+    return this.options.hasLabels;
   }
 
   setLabels(label: boolean): void {
-    this.labels = label;
+    this.options.hasLabels = label;
     this.notifyPresenter({
       hasLabels: this.getLabels(),
     });
@@ -150,7 +149,7 @@ export default class MainModel {
 
   verifyValue(value: number): number {
     let checkedValue;
-    const modulus = (this.getMax() - this.getMin()) % this.stepValue;
+    const modulus = (this.getMax() - this.getMin()) % this.options.step;
     if (modulus > 0 && (value + this.getMin()) > this.getMax() - modulus) {
       checkedValue = this.getMax() - modulus;
       return checkedValue;
@@ -160,25 +159,25 @@ export default class MainModel {
         checkedValue = this.getMax() - modulus;
         return checkedValue;
       }
-      const shift = Math.abs(this.getMin()) - Math.round(Math.abs(this.getMin()) / this.stepValue) * this.stepValue;
-      checkedValue = Math.round(value / this.stepValue) * this.stepValue - shift;
+      const shift = Math.abs(this.getMin()) - Math.round(Math.abs(this.getMin()) / this.options.step) * this.options.step;
+      checkedValue = Math.round(value / this.options.step) * this.options.step - shift;
       return checkedValue;
     }
-    if (value === this.values[0] || value === this.values[1]) {
+    if (value === this.options.values[0] || value === this.options.values[1]) {
       return value;
     }
-    checkedValue = Math.round(value / this.stepValue) * this.stepValue + this.getMin();
+    checkedValue = Math.round(value / this.options.step) * this.options.step + this.getMin();
     return checkedValue;
   }
 
   verifyMinMaxValues(): void {
-    if (this.values[0] > this.values[1]) {
-      [this.values[0], this.values[1]] = [this.values[1], this.values[0]];
+    if (this.options.values[0] > this.options.values[1]) {
+      [this.options.values[0], this.options.values[1]] = [this.options.values[1], this.options.values[0]];
     }
   }
 
   verifyLimits(): void {
-    this.values = this.values.map((value) => (value < this.getMin() ? this.getMin() : value > this.getMax() ? this.getMax() : value));
+    this.options.values = this.options.values.map((value) => (value < this.getMin() ? this.getMin() : value > this.getMax() ? this.getMax() : value));
   }
 
   verifyStep(step: number): number {
