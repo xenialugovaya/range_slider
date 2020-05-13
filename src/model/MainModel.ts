f02ca132e5bc3b2a6ddb1d9b99f531547bb17949
Ksenia Lugovaya <xenia.trofimova@gmail.com>
2020-05-13 15:29:24 +0300

    #1 renamed notifyPresenter method in Model

--- src/model/MainModel.ts
+++ src/model/MainModel.ts
@@ -18,6 +18,121 @@ export default class MainModel {
     this.init(options);
   }
 
+  public update(valueData: sliderOptions): void {
+    if (valueData.min !== undefined) this.setMin(valueData.min);
+    if (valueData.max) this.setMax(valueData.max);
+    if (valueData.values) this.setValues(valueData.values);
+    if (valueData.isVertical !== undefined) this.setOrientation(valueData.isVertical);
+    if (valueData.step) this.setStep(valueData.step);
+    if (valueData.hasRange !== undefined) this.setRange(valueData.hasRange);
+    if (valueData.hasLabels !== undefined) this.setLabels(valueData.hasLabels);
+  }
+
+  public getMin(): number {
+    if (this.options.min > this.options.max) {
+      return this.options.max;
+    }
+    return this.options.min;
+  }
+
+  public setMin(min: number): void {
+    this.options.min = min;
+    this.broadcastUpdates({
+      min: this.getMin(),
+      max: this.getMax(),
+      values: this.getValues(),
+      step: this.getStep(),
+    });
+  }
+
+  public getMax(): number {
+    if (this.options.max < this.options.min) {
+      return this.options.min;
+    }
+    if (this.options.max === this.options.min) {
+      return this.options.max + this.options.step;
+    }
+    return this.options.max;
+  }
+
+  public setMax(max: number): void {
+    this.options.max = max;
+    this.broadcastUpdates({
+      min: this.getMin(),
+      max: this.getMax(),
+      values: this.getValues(),
+      step: this.getStep(),
+    });
+  }
+
+  public getStep(): number {
+    this.options.step = this.verifyStep(this.options.step);
+    return this.options.step;
+  }
+
+  public setStep(step: number): void {
+    this.options.step = this.verifyStep(step);
+    this.broadcastUpdates({
+      step: this.getStep(),
+      values: this.getValues(),
+    });
+  }
+
+  public getValues(): number[] {
+    this.options.values = this.options.values.map((value) => this.verifyValue(value));
+    this.verifyMinMaxValues();
+    this.verifyLimits();
+    return this.options.values;
+  }
+
+  public setValues(values: number[]): void {
+    values.forEach((value, index) => {
+      if (value !== this.options.values[index]) {
+        this.options.values[index] = this.verifyValue(value);
+      }
+    });
+    this.verifyMinMaxValues();
+    this.verifyLimits();
+    this.broadcastUpdates({
+      values: this.options.values,
+    });
+  }
+
+  public getOrientation(): boolean {
+    return this.options.isVertical;
+  }
+
+  public setOrientation(vertical: boolean): void {
+    this.options.isVertical = vertical;
+    this.broadcastUpdates({
+      values: this.getValues(),
+      isVertical: this.getOrientation(),
+    });
+  }
+
+  public getRange(): boolean {
+    return this.options.hasRange;
+  }
+
+  public setRange(range: boolean): void {
+    this.options.hasRange = range;
+    this.broadcastUpdates({
+      values: this.getValues(),
+      hasRange: this.getRange(),
+    });
+  }
+
+  public getLabels(): boolean {
+    return this.options.hasLabels;
+  }
+
+  public setLabels(label: boolean): void {
+    this.options.hasLabels = label;
+    this.broadcastUpdates({
+      hasLabels: this.getLabels(),
+    });
+  }
+
   private init(options: sliderOptions): void {
     this.options.max = options.max ? options.max : this.options.max;
     this.options.min = options.min ? options.min : this.options.min;
@@ -28,126 +143,7 @@ export default class MainModel {
     this.options.hasLabels = options.hasLabels ? options.hasLabels : this.options.hasLabels;
   }
 
-  update(valueData: sliderOptions): void {
-    if (valueData.min !== undefined) this.setMin(valueData.min);
-    if (valueData.max) this.setMax(valueData.max);
-    if (valueData.values) this.setValues(valueData.values);
-    if (valueData.isVertical !== undefined) this.setOrientation(valueData.isVertical);
-    if (valueData.step) this.setStep(valueData.step);
-    if (valueData.hasRange !== undefined) this.setRange(valueData.hasRange);
-    if (valueData.hasLabels !== undefined) this.setLabels(valueData.hasLabels);
-  }
-
-  notifyPresenter(valueData: sliderOptions): void {
-    this.observer.broadcast(valueData);
-  }
-
-  getMin(): number {
-    if (this.options.min > this.options.max) {
-      return this.options.max;
-    }
-    return this.options.min;
-  }
-
-  setMin(min: number): void {
-    this.options.min = min;
-    this.notifyPresenter({
-      min: this.getMin(),
-      max: this.getMax(),
-      values: this.getValues(),
-      step: this.getStep(),
-    });
-  }
-
-  getMax(): number {
-    if (this.options.max < this.options.min) {
-      return this.options.min;
-    }
-    if (this.options.max === this.options.min) {
-      return this.options.max + this.options.step;
-    }
-    return this.options.max;
-  }
-
-  setMax(max: number): void {
-    this.options.max = max;
-    this.notifyPresenter({
-      min: this.getMin(),
-      max: this.getMax(),
-      values: this.getValues(),
-      step: this.getStep(),
-    });
-  }
-
-  getStep(): number {
-    this.options.step = this.verifyStep(this.options.step);
-    return this.options.step;
-  }
-
-  setStep(step: number): void {
-    this.options.step = this.verifyStep(step);
-    this.notifyPresenter({
-      step: this.getStep(),
-      values: this.getValues(),
-    });
-  }
-
-  getValues(): number[] {
-    this.options.values = this.options.values.map((value) => this.verifyValue(value));
-    this.verifyMinMaxValues();
-    this.verifyLimits();
-    return this.options.values;
-  }
-
-  setValues(values: number[]): void {
-    values.forEach((value, index) => {
-      if (value !== this.options.values[index]) {
-        this.options.values[index] = this.verifyValue(value);
-      }
-    });
-    this.verifyMinMaxValues();
-    this.verifyLimits();
-    this.notifyPresenter({
-      values: this.options.values,
-    });
-  }
-
-  getOrientation(): boolean {
-    return this.options.isVertical;
-  }
-
-  setOrientation(vertical: boolean): void {
-    this.options.isVertical = vertical;
-    this.notifyPresenter({
-      values: this.getValues(),
-      isVertical: this.getOrientation(),
-    });
-  }
-
-  getRange(): boolean {
-    return this.options.hasRange;
-  }
-
-  setRange(range: boolean): void {
-    this.options.hasRange = range;
-    this.notifyPresenter({
-      values: this.getValues(),
-      hasRange: this.getRange(),
-    });
-  }
-
-  getLabels(): boolean {
-    return this.options.hasLabels;
-  }
-
-  setLabels(label: boolean): void {
-    this.options.hasLabels = label;
-    this.notifyPresenter({
-      hasLabels: this.getLabels(),
-    });
-  }
-
-  verifyValue(value: number): number {
+  private verifyValue(value: number): number {
     let checkedValue;
     const modulus = (this.getMax() - this.getMin()) % this.options.step;
     if (modulus > 0 && (value + this.getMin()) > this.getMax() - modulus) {
@@ -170,17 +166,17 @@ export default class MainModel {
     return checkedValue;
   }
 
-  verifyMinMaxValues(): void {
+  private verifyMinMaxValues(): void {
     if (this.options.values[0] > this.options.values[1]) {
       [this.options.values[0], this.options.values[1]] = [this.options.values[1], this.options.values[0]];
     }
   }
 
-  verifyLimits(): void {
+  private verifyLimits(): void {
     this.options.values = this.options.values.map((value) => (value < this.getMin() ? this.getMin() : value > this.getMax() ? this.getMax() : value));
   }
 
-  verifyStep(step: number): number {
+  private verifyStep(step: number): number {
     const maxStep = this.getMax() - this.getMin();
     if (step > maxStep) {
       return maxStep;
@@ -188,5 +184,9 @@ export default class MainModel {
       return 1;
     }
     return step;
+  }
+
+  private broadcastUpdates(valueData: sliderOptions): void {
+    this.observer.broadcast(valueData);
   }
 }
