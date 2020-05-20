857368d0d1c56c82d8234c74a4b2b9acf24134ae
Ksenia Lugovaya <xenia.trofimova@gmail.com>
2020-05-20 13:46:49 +0300

    #1 removed numbers from variables names

--- src/demo/demo.ts
+++ src/demo/demo.ts
@@ -1,9 +1,9 @@
 import './demo.scss';
 import SliderInit from './SliderInit';
 
-const slider4 = new SliderInit({
+const sliderFour = new SliderInit({
   max: 100, min: 10, step: 10, values: [19], hasLabels: true, hasRange: false, isVertical: false,
 });
-const slider3 = new SliderInit({ max: 200, isVertical: true });
-const slider2 = new SliderInit({ max: 200 });
-const slider1 = new SliderInit({ max: 100, hasRange: true });
+const sliderThree = new SliderInit({ max: 200, isVertical: true });
+const sliderTwo = new SliderInit({ max: 200 });
+const sliderOne = new SliderInit({ max: 100, hasRange: true });
