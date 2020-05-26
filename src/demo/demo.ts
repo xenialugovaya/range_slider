b893e8011299e3170304157f06219e8ac236250b
Ksenia Lugovaya <xenia.trofimova@gmail.com>
2020-05-26 12:14:58 +0300

    #5 added refactored initialization

--- src/demo/demo.ts
+++ src/demo/demo.ts
@@ -1,9 +1,27 @@
 import './demo.scss';
 import SliderInit from './SliderInit';
 
-const sliderFour = new SliderInit({
-  max: 100, min: 10, step: 10, values: [19], hasLabels: true, hasRange: false, isVertical: false,
-});
-const sliderThree = new SliderInit({ max: 200, isVertical: true });
-const sliderTwo = new SliderInit({ max: 200 });
-const sliderOne = new SliderInit({ max: 100, hasRange: true });
+const config = [
+  {
+    max: 100,
+    min: 10,
+    step: 10,
+    values: [19],
+    hasLabels: true,
+    hasRange: false,
+    isVertical: false,
+  },
+  {
+    max: 200,
+    isVertical: true,
+  },
+  {
+    max: 200,
+  },
+  {
+    max: 100,
+    hasRange: true,
+  },
+];
+
+config.forEach((slider) => new SliderInit(slider));
