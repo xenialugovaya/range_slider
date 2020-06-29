9078a75d26c3f6c6093743236c2847e7c2c38878
Ksenia Lugovaya <xenia.trofimova@gmail.com>
2020-06-29 09:55:11 +0300

    #8 fixed tests

--- tests/src/model/Validators.spec.ts
+++ tests/src/model/Validators.spec.ts
@@ -1,6 +1,5 @@
 import Validators from '../../../src/model/Validators';
 
-
 describe('test Validators logic', () => {
   const testOptions = {
     newValue: 9,
@@ -11,72 +10,70 @@ describe('test Validators logic', () => {
     step: 5,
   };
 
+  beforeEach(() => {
+    testOptions.min = 1;
+    testOptions.newValue = 9;
+    testOptions.step = 5;
+  });
+
   describe('test verifyValue function', () => {
     it('if step is bigger than remained segment, the handler should not move', () => {
-      const {
-        newValue, index, currentValues, min, max, step,
-      } = testOptions;
+      const { min, max, step } = testOptions;
       const modulus = (max - min) % step;
-      expect(Validators.verifyValue(newValue, index, currentValues, min, max, step)).toEqual(max - modulus);
+      expect(Validators.verifyValue(testOptions)).toEqual(max - modulus);
     });
     it('if min is less than 0 and step is bigger than remained segment, the handler should not move', () => {
-      let {
-        newValue, index, currentValues, min, max, step,
-      } = testOptions;
-      min = -4;
+      testOptions.min = -4;
+      const { max, min, step } = testOptions;
       const modulus = (max - min) % step;
-      expect(Validators.verifyValue(newValue, index, currentValues, min, max, step)).toEqual(max - modulus);
+      expect(Validators.verifyValue(testOptions)).toEqual(max - modulus);
     });
 
     it('if min is less than 0, newValue < currentValues[index] and shift < 0 value should be counted with Math.round', () => {
-      let {
-        newValue, index, currentValues, min, max, step,
-      } = testOptions;
-      min = -4;
-      newValue = -3;
+      testOptions.min = -4;
+      testOptions.newValue = -3;
+      const { newValue, min, step } = testOptions;
       const shift = Math.abs(min) - Math.round(Math.abs(min) / step) * step;
-      expect(Validators.verifyValue(newValue, index, currentValues, min, max, step)).toEqual(Math.round(newValue / step) * step - shift);
+      expect(Validators.verifyValue(testOptions)).toEqual(
+        Math.round(newValue / step) * step - shift,
+      );
     });
 
     it('if min is less than 0, newValue < currentValues[index] and shift >= 0 value should be counted with Math.round', () => {
-      let {
-        newValue, index, currentValues, min, max, step,
-      } = testOptions;
-      min = -5;
-      newValue = -3;
+      testOptions.min = -5;
+      testOptions.newValue = -3;
+      const { newValue, min, step } = testOptions;
       const shift = Math.abs(min) - Math.round(Math.abs(min) / step) * step;
-      expect(Validators.verifyValue(newValue, index, currentValues, min, max, step)).toEqual(Math.ceil(newValue / step) * step - shift);
+      expect(Validators.verifyValue(testOptions)).toEqual(
+        Math.ceil(newValue / step) * step - shift,
+      );
     });
 
     it('if min is less than 0 and newValue > currentValues[index] value should be counted with Math.floor', () => {
-      let {
-        newValue, index, currentValues, min, max, step,
-      } = testOptions;
-      min = -4;
+      testOptions.min = -4;
+      const { newValue, min, step } = testOptions;
       const shift = Math.abs(min) - Math.round(Math.abs(min) / step) * step;
-      expect(Validators.verifyValue(newValue, index, currentValues, min, max, step)).toEqual(Math.floor(newValue / step) * step - shift);
+      expect(Validators.verifyValue(testOptions)).toEqual(
+        Math.floor(newValue / step) * step - shift,
+      );
     });
 
     it('if selected segment is not divided evenly by step, the value should be adjusted on min limit', () => {
-      let {
-        newValue, index, currentValues, min, max, step,
-      } = testOptions;
-      step = 4;
-      expect(Validators.verifyValue(newValue, index, currentValues, min, max, step)).toEqual(Math.round(newValue / step) * step + min);
+      testOptions.step = 4;
+      const { newValue, min, step } = testOptions;
+      expect(Validators.verifyValue(testOptions)).toEqual(Math.round(newValue / step) * step + min);
     });
   });
 
   describe('test verifyStep function', () => {
-    let {
-      min, max, step,
-    } = testOptions;
-
     it('step should not be bigger than max - min', () => {
-      step = 20;
+      testOptions.step = 20;
+      const { min, max, step } = testOptions;
       expect(Validators.verifyStep(step, min, max)).toEqual(max - min);
     });
     it('step should not be less than 1', () => {
-      step = -1;
+      testOptions.step = -1;
+      const { min, max, step } = testOptions;
       expect(Validators.verifyStep(step, min, max)).toEqual(1);
     });
   });
